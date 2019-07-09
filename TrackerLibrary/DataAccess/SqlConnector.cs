06afb32b264e0793203a40cad32cdf033cda889b
Slavik <slavik.kuz.mail@gmail.com>
2019-07-09 18:53:28 +0200

    cleanup

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
-    public class TextConnection : IDataConnection
+    public class SqlConnector : IDataConnection
     {
         //TODO - make CreatePrize to save to db
         /// <summary>
-        /// saves a new prize to the txt
+        /// saves a new prize to the DB
         /// </summary>
         /// <param name="model">prize info</param>
         /// <returns>The prize info, including the unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             model.Id = 1;
+
             return model;
         }
     }
--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
-    public class SqlConnector : IDataConnection
+    public class TextConnection : IDataConnection
     {
         //TODO - make CreatePrize to save to db
         /// <summary>
-        /// saves a new prize to the DB
+        /// saves a new prize to the txt
         /// </summary>
         /// <param name="model">prize info</param>
         /// <returns>The prize info, including the unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             model.Id = 1;
-
             return model;
         }
     }
