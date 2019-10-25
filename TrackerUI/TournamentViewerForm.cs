5a35e83f92f2a3b9ea9b7737530d953e0d08a974
Slavik <slavik.kuz.mail@gmail.com>
2019-10-25 13:58:40 +0200

    error handling

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -157,6 +157,14 @@ namespace TrackerUI
 
         private void scoreButton_Click(object sender, EventArgs e)
         {
+            string errorMessage = ValidateData();
+
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show($"Input Error: { errorMessage }");
+                return;
+            }
+
             MatchupModel m = (MatchupModel)matchUpListBox.SelectedItem;
             double teamOneScore = 0;
             double teamTwoScore = 0;
@@ -200,9 +208,46 @@ namespace TrackerUI
                     }
                 }
 
-            TournamentLogic.UpdateTounamentResults(tournament);  
+            try
+            {
+                TournamentLogic.UpdateTounamentResults(tournament);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The application had the following error: {ex.Message}");
+            }
 
             LoadMatchups((int)roundDropDown.SelectedItem);
+        }
+
+        private string ValidateData()
+        {
+            string output = "";
+
+            double teamOneScore = 0;
+            double teamTwoScore = 0;
+
+            bool scoreOneValid = double.TryParse(teamOneScoreValue.Text, out teamOneScore);
+            bool scoreTwoValid = double.TryParse(teamTwoScoreValue.Text, out teamTwoScore);
+
+            if (!scoreOneValid)
+            {
+                output = "The Score One value is not a valid number.";
+            }
+            else if (!scoreTwoValid)
+            {
+                output = "The Score Two value is not a valid number.";
+            }
+            else if (teamOneScore == 0 && teamTwoScore == 0)
+            {
+                output = "You did not enter a score for either team.";
+            }
+            else if (teamOneScore == teamTwoScore)
+            {
+                output = "Ties are not allowed in the app.";
+            }
+
+            return output;
         }
     }
 }
